5e7c9b50b388fff1af95cee8e8e39828020bef80
Ümit Tunç <umitreva@gmail.com>
2023-10-20 20:42:43 +0300

    setting for section in index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,49 +32,30 @@
         <div class="container position-relative">
             <img src="template/assets/images/service/dot-shape.png" class="shape position-absolute">
             <div class="row">
-                <div class="col-12"><small class="fs-7 d-block">Working Process</small></div>
+                    <div class="col-12"><small class="fs-7 d-block">{{setting("hizmetlerimiz-small")}}</small></div>
                 <div class="col-12 d-xxl-flex d-xl-flex d-lg-flex d-md-flex d-sm-block d-block align-items-center justify-content-xxl-between justify-content-xl-between justify-content-lg-between justify-content-md-between justify-content-sm-between justify-content-sm-center ">
-                    <h2 class="fs-2 text-black mb-0">Our Featured Service<br> that We Provide</h2>
-                    <a href="#" class="btn btn-warning btn-hover-secondery section-btn">All Services</a>
+                    <h2 class="fs-2 text-black mb-0">{{setting("hizmetlerimiz-big")}}</h2>
+                    <a href="{{url("hizmetlerimiz")}}" class="d-none btn btn-warning btn-hover-secondery section-btn">All Services</a>
                 </div>
             </div>
             <div class="row d-flex flex-wrap justify-content-center step-row">
-                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 text-center">
-                    <div class="card border-0 shadow">
-                        <div class="card-body">
-                            <div class="icon-round overflow-hidden rounded-circle position-relative d-flex align-items-center justify-content-center mx-auto text-center">
-                                <i class="ti ti-download text-primary position-relative"></i>
-                            </div>
-                            <h5 class="mb-0 fw-500">Step 1</h5>
-                            <h3 class="fs-4">Download our App</h3>
-                            <p class="fs-7 mb-0 fw-500">There are many variations of pass for ages of oremsum.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 text-center">
-                    <div class="card border-0 shadow">
-                        <div class="card-body">
-                            <div class="icon-round overflow-hidden rounded-circle position-relative d-flex align-items-center justify-content-center mx-auto text-center">
-                                <i class="ti ti-user text-primary position-relative"></i>
-                            </div>
-                            <h5 class="mb-0 fw-500">Step 2</h5>
-                            <h3 class="fs-4">Create a free Account</h3>
-                            <p class="fs-7 mb-0 fw-500">There are many variations of pass for ages of oremsum.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 text-center">
-                    <div class="card border-0 shadow">
-                        <div class="card-body">
-                            <div class="icon-round overflow-hidden rounded-circle position-relative d-flex align-items-center justify-content-center mx-auto text-center">
-                                <i class="ti ti-gift text-primary position-relative"></i>
-                            </div>
-                            <h5 class="mb-0 fw-500">Step 3</h5>
-                            <h3 class="fs-4">Now Start your Journey</h3>
-                            <p class="fs-7 mb-0 fw-500">There are many variations of pass for ages of oremsum.</p>
-                        </div>
-                    </div>
-                </div>
+                <?php $servisler = contents("hizmetlerimiz", 0, 3);
+                foreach($servisler AS $servis)  { 
+                 
+                 ?>
+                 <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 text-center">
+                     <div class="card border-0 shadow">
+                         <div class="card-body">
+                            <a href="{{$servis->slug}}">
+                             <img src="{{picture2($servis->cover, 256)}}" width="256" alt="">
+                             <h3 class="fs-4">{{e2($servis->title)}}</h3>
+                             <p class="fs-7 mb-0 fw-500">{{e2(short_text($servis->html,50))}}</p>
+                             </a>
+                         </div>
+                     </div>
+                 </div> 
+                 <?php } ?>
+                
             </div>
         </div>
     </section>
