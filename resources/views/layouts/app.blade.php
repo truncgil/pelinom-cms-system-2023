bffd7715659f365832a0ee6c1ef79a19a0daf6f6
Ümit Tunç <umitreva@gmail.com>
2024-01-15 18:38:37 +0300

    link düzeltme

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -186,7 +186,7 @@
     <!-------Footer End------------->
     <!------------------------------>
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-K8WCC37H15"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id={{env('GOOGLE_ANALYTICS')}}"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
